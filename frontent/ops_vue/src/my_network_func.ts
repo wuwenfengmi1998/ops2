a0503d575ea6ec076b49877d0b98510f986f37bc
kevin <kevin@lmve.net>
2025-11-04 20:50:06 +0800

    up

---
+++ frontent/ops_vue/src/my_network_func.ts
@@ -0,0 +1,4 @@
+
+export const my_network_func = {
+
+}
--- frontent/ops_vue/vitest.config.ts
+++ frontent/ops_vue/vitest.config.ts
@@ -10,5 +10,13 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8080',
+          changeOrigin: true,
+        },
+      },
+    },
   }),
 )
